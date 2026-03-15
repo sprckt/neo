e909344cdb986bcd0f12673a305cfe28e6d1c72d
Eric Thanenthiran <et@sg-macbook-1>
2026-03-15 23:30:45 +0100

    Updated some theming I think

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -194,4 +194,21 @@ return {
       },
     },
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = true,
+      },
+    },
+  },
 }
