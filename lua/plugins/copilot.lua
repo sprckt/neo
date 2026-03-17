bd828620f53286d69d72858a0f59d8f37e6a79b3
Eric Thanenthiran <et@tas.pro>
2026-03-17 18:40:32 +0100

    Trying to get rid of Markdown auto suggest

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,9 +1,17 @@
 -- Copilot configuration
 return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    filetypes = {
-      markdown = false,
+  -- Standard copilot
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = {
+        markdown = false,
+      },
     },
+  },
+  -- Native copilot
+  {
+    "github/copilot.vim",
+    enabled = false,
   },
 }
